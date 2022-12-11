b3c9460f04b986533fb31705dd54ef5465eb68de
kostenchcick <kostenchick1211@gmail.com>
2022-12-11 20:35:24 +0100

    class Component

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// class HeaderComponent extends React.Component {
-//     render() {
-//         console.log(this.props);
-//         const p = React.createElement('p', {}, 'Super paragraph');
-//         const h2 = React.createElement('h2', {title: 'Hi!'}, `Hello ${this.props.text}` );
-//         const article = React.createElement('article', {}, h2, p);
-//         return article;
-//     }
+class HeaderComponent extends React.Component {
+    render() {
+        const p = React.createElement('p', {}, 'Super paragraph');
+        const h2 = React.createElement('h2', {}, 'Header');
+        const article = React.createElement('atricle', {}, h2, p); 
+        return article;
+    }
+}
 
-//     const component = React.createElement(HeaderComponent, {text: 'React!!!'});
-//     const component2 = React.createElement(HeaderComponent, {text: 'JavaScript'});
-//     const parentElement = 
-// }
+const component = React.createElement(HeaderComponent);
 
 const root = document.querySelector('#root');
-const h1 = React.createElement('h1', {}, 'Hello React!');
-
-ReactDOM.render(h1, root); // прицепить, заапендить = монтировать
+ReactDOM.render(component, root);
