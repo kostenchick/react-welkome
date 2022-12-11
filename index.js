06b797fe532ae2a777f52bbaeb50149986d209d2
kostenchcick <kostenchick1211@gmail.com>
2022-12-11 21:48:55 +0100

    State

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-class HeaderComponent extends React.Component {
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment() {
+        this.setState({
+            count: this.state.count+1
+        });
+    }
+
+    decrement() {
+        if(this.state.count > 0) {
+            this.setState({
+                count: this.state.count-1
+            });
+        } else if(this.state.count === 0) {
+            this.setState({
+                count: this.state.count
+            })
+        }
+        
+    }
+
     render() {
-        console.log(this.props);
-        const p = React.createElement('p', {}, 'Super paragraph');
-        const h2 = React.createElement('h2', {title: 'Hi!'}, `Hello ${this.props.name}`);
-        const article = React.createElement('atricle', {}, h2, p); 
-        return article;
+        const h2 = React.createElement('h2', {}, this.state.count);
+        const buttonDecrement = React.createElement('button', {onClick: () => {this.decrement()}}, '-')
+        const buttonIncrement = React.createElement('button', {title: 'Hello', onClick: () => {this.increment()}}, '+');
+        return React.createElement(React.Fragment, null, h2, buttonDecrement, buttonIncrement);
     }
 }
 
-const component = React.createElement(HeaderComponent, {name: 'React!11!'}, 'Text');
-const component2 = React.createElement(HeaderComponent, {name: 'JavaScript'});
-const parentElement = React.createElement('section', {}, component, component2);
+const component = React.createElement(Counter);
 
 const root = document.querySelector('#root');
 ReactDOM.render(component, root);
-ReactDOM.render(parentElement, root);
+
+/*
+Базовий рівень.
+Зробіть такий же counter, який буде крі інкременту вміти ще декрементувати лічильник.
+Продвинутий рівень.
+Лічильник не має опускатись нижче нуля
+*/
